2326b407caa96c9313714892942c1409c6fa30bd
RachidLadjRd <rachid.ladjouzi@loreal.com>
2024-10-18 17:18:58 +0200

    ADD: configure cookiecutter and hooks to set remote url to push new project

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,3 +3,4 @@ import subprocess
 subprocess.call(['git', 'init'])
 subprocess.call(['git', 'add', '.'])
 subprocess.call(['git', 'commit', '-m', 'Initial commit_'])
+subprocess.call(['git', 'remote', 'add', 'origin', "{{cookiecutter.github_repo_link}}"])
